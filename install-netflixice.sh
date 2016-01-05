5ede4f03dd3de47334a4aa10d183e4f43baacafd
Naveen <naveenlj@users.noreply.github.com>
2016-01-06 03:53:27 +0530

    Update install-netflixice.sh

--- install-netflixice.sh
+++ install-netflixice.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 # Installing Netflix/ice
-
 set -x
 
 # Root check
@@ -15,8 +14,17 @@ if  [ $? != 0 ]; then
   yum -y install git >/dev/null 2>&1
 fi
 
+wget https://gitlab.com/naveenlj/java-install/raw/master/install-java8.sh
+
+bash install-java8.sh
+
+JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk.x86_64
+
+export JAVA_HOME
+
 git clone https://github.com/Netflix/ice.git
 
 cd ice
 
 bash install.sh
+
